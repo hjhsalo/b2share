69616817d844626f9846697acc13361ce798ae69
hjhsalo <>
2020-08-09 17:41:41 +0300

    fix: provide upgrade path from 2.1.3 -> 2.1.5
    Signed-off-by: hjhsalo <harri.hirvonsalo@csc.fi>

--- b2share/modules/upgrade/upgrades/upgrade_2_1_3_to_2_1_5.py
+++ b2share/modules/upgrade/upgrades/upgrade_2_1_3_to_2_1_5.py
@@ -21,7 +21,7 @@
 # waive the privileges and immunities granted to it by virtue of its status
 # as an Intergovernmental Organization or submit itself to any jurisdiction.
 
-"""Upgrade recipe migrating B2SHARE from version 2.1.3 to 2.1.4."""
+"""Upgrade recipe migrating B2SHARE from version 2.1.3 to 2.1.5."""
 
 
 from __future__ import absolute_import, print_function
@@ -31,7 +31,7 @@ import pkg_resources
 from ..api import UpgradeRecipe
 
 
-migrate_2_1_3_to_2_1_4 = UpgradeRecipe('2.1.3', '2.1.4')
+migrate_2_1_3_to_2_1_5 = UpgradeRecipe('2.1.3', '2.1.5')
 
 # No changes to Elasticsearch mappings
 
